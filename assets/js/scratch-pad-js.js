2060de63c46e8411a533fd346c7239a243319da5
Ishan Vyas <vyasishanatc194@gmail.com>
2020-12-28 15:15:38 +0530

    Working in home page

--- assets/js/scratch-pad-js.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("#scratch-box-item-btn").click(function () {
-    $(this).parent().toggleClass("active");
-    $("#scratch-pad-root").toggleClass("active");
-    $("#scratch-pad-active-main").toggleClass("active");
-    $("#scratch-pad-active-header").toggleClass("active");
-  });
-});
